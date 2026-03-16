b2fe557ba9f95ed96d4c8c4c8cac5e96fdae3af7
Robin Gosse <robingosse2@gmail.com>
2026-03-16 09:59:09 -0230

    Update keymap.c

--- keyboards/cantor/keymaps/vial/keymap.c
+++ keyboards/cantor/keymaps/vial/keymap.c
@@ -19,10 +19,10 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
       *                       └───┘   └───┘
       */
     [0] = LAYOUT_split_3x6_3(
-        KC_NO,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,                               KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_NO,
-        KC_NO, KC_A,    KC_S,    KC_D,    KC_F,    KC_G,                               KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_NO,
-        KC_NO, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,                               KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_NO,
-                                            KC_LGUI, KC_SPC, KC_BSPC,          KC_ENT, KC_SPC, KC_RALT
+        KC_NO, KC_N, KC_D, KC_P, KC_L, KC_X,                                        KC_X, KC_T, KC_J, KC_Q, KC_U, KC_NO,
+        KC_NO, KC_R, KC_S, KC_H, KC_W, KC_B,                                        KC_E, KC_O, KC_Y, KC_G, KC_L, KC_NO,
+        KC_NO, KC_Z, KC_X, KC_C, KC_V, KC_X,                                        KC_X, KC_A, KC_I, KC_K, KC_M, KC_NO,
+                                 KC_LGUI, KC_SPC, KC_BSPC,          KC_ENT, KC_SPC, KC_RALT
     ),
 
     [1] = LAYOUT_split_3x6_3(
