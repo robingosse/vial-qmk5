e9b08be4c183adba58d2efdf01e3d356d2672e7b
Robin Gosse <robingosse2@gmail.com>
2026-05-11 14:48:07 -0230

    Implement custom matrix scanner in matrix.c
    Added custom matrix scanner implementation.

--- keyboards/handwired/onekey/rp2040/matrix.c
+++ keyboards/handwired/onekey/rp2040/matrix.c
@@ -1,3 +1,15 @@
-Rows 0-2: left MCP23017 half
-Rows 3-5: right RP2040 half
-Cols 0-5 shared logical columns
+// DacMan custom matrix scanner
+// Rows 0-2: left MCP23017 half
+// Rows 3-5: right RP2040 half
+// Cols 0-5: shared logical columns
+
+#include "quantum.h"
+#include "matrix.h"
+#include "i2c_master.h"
+
+void matrix_init_custom(void) {
+}
+
+bool matrix_scan_custom(matrix_row_t current_matrix[]) {
+    return false;
+}
