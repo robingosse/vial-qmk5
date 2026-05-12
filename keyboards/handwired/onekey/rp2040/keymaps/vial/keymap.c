b1d04abd9c13fc81978e12717c98d3fb12a69142
Robin Gosse <robingosse2@gmail.com>
2026-05-12 12:16:44 -0230

    Update keymap.c

--- keyboards/handwired/onekey/rp2040/keymaps/vial/keymap.c
+++ keyboards/handwired/onekey/rp2040/keymaps/vial/keymap.c
@@ -1,5 +1,15 @@
 #include QMK_KEYBOARD_H
 
+#ifdef VIAL_ENABLE
+
+combo_t key_combos[1];
+
+tap_dance_action_t tap_dance_actions[1];
+
+const key_override_t **key_overrides = NULL;
+
+#endif
+
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     [0] = LAYOUT(
         KC_Q, KC_W, KC_E, KC_R, KC_T, KC_Y,
