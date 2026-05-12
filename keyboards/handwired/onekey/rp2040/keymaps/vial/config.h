a074522f7818a7a78e85c4a8d983809cdf0356de
Robin Gosse <robingosse2@gmail.com>
2026-05-12 15:08:40 -0230

    Update config.h

--- keyboards/handwired/onekey/rp2040/keymaps/vial/config.h
+++ keyboards/handwired/onekey/rp2040/keymaps/vial/config.h
@@ -1,8 +1,6 @@
 #pragma once
 
-#define MATRIX_ROWS 6
-#define MATRIX_COLS 6
+#define VIAL_KEYBOARD_UID {0xD4, 0xC0, 0x00, 0x00, 0x24, 0x05, 0x20, 0x26}
 
-#define I2C_DRIVER I2CD1
-#define I2C1_SDA_PIN GP28
-#define I2C1_SCL_PIN GP29
+#define VIAL_UNLOCK_COMBO_ROWS {3, 3}
+#define VIAL_UNLOCK_COMBO_COLS {0, 5}
