278eb2851a9af487319d9f476375b6d52bb16b6f
Robin Gosse <robingosse2@gmail.com>
2026-05-12 15:28:58 -0230

    Update matrix.c

--- keyboards/handwired/onekey/rp2040/matrix.c
+++ keyboards/handwired/onekey/rp2040/matrix.c
@@ -1,137 +1,17 @@
 #include "quantum.h"
 #include "matrix.h"
-#include "i2c_master.h"
 #include "wait.h"
 
-#define MCP23017_ADDR 0x40
-
-#define MCP_IODIRA 0x00
-#define MCP_IODIRB 0x01
-#define MCP_GPPUA  0x0C
-#define MCP_GPPUB  0x0D
-#define MCP_GPIOA  0x12
-#define MCP_GPIOB  0x13
-#define MCP_OLATA  0x14
-#define MCP_OLATB  0x15
-
-static const pin_t right_row_pins[3] = { GP0, GP2, GP4 };
-static const pin_t right_col_pins[6] = { GP6, GP8, GP10, GP12, GP14, GP26 };
-
-static bool mcp_available = false;
-
-static bool mcp_write(uint8_t reg, uint8_t value) {
-    uint8_t data[2] = { reg, value };
-    return i2c_transmit(MCP23017_ADDR, data, 2, 5) == I2C_STATUS_SUCCESS;
-}
-
-static bool mcp_read(uint8_t reg, uint8_t *value) {
-    *value = 0xFF;
-
-    if (i2c_transmit(MCP23017_ADDR, &reg, 1, 5) != I2C_STATUS_SUCCESS) {
-        return false;
-    }
-
-    if (i2c_receive(MCP23017_ADDR, value, 1, 5) != I2C_STATUS_SUCCESS) {
-        *value = 0xFF;
-        return false;
-    }
-
-    return true;
-}
-
-static void select_left_row(uint8_t row) {
-    if (!mcp_available) return;
-
-    uint8_t iodira = 0xFF;
-    uint8_t olata  = 0xFF;
-
-    iodira &= ~(1 << row);
-    olata  &= ~(1 << row);
-
-    mcp_write(MCP_IODIRA, iodira);
-    mcp_write(MCP_OLATA, olata);
-
-    mcp_write(MCP_IODIRB, 0xFF);
-    mcp_write(MCP_OLATB, 0xFF);
-}
-
-static matrix_row_t read_left_cols(void) {
-    if (!mcp_available) return 0;
-
-    uint8_t gpioa = 0xFF;
-    uint8_t gpiob = 0xFF;
-
-    if (!mcp_read(MCP_GPIOA, &gpioa)) {
-        mcp_available = false;
-        return 0;
-    }
-
-    if (!mcp_read(MCP_GPIOB, &gpiob)) {
-        mcp_available = false;
-        return 0;
-    }
-
-    matrix_row_t row = 0;
-
-    if (!(gpioa & (1 << 3))) row |= (1 << 0);
-    if (!(gpioa & (1 << 4))) row |= (1 << 1);
-    if (!(gpioa & (1 << 5))) row |= (1 << 2);
-    if (!(gpioa & (1 << 6))) row |= (1 << 3);
-    if (!(gpioa & (1 << 7))) row |= (1 << 4);
-    if (!(gpiob & (1 << 7))) row |= (1 << 5);
-
-    return row;
-}
-
-static void mcp_all_idle(void) {
-    if (!mcp_available) return;
-
-    mcp_write(MCP_IODIRA, 0xFF);
-    mcp_write(MCP_IODIRB, 0xFF);
-    mcp_write(MCP_OLATA, 0xFF);
-    mcp_write(MCP_OLATB, 0xFF);
-}
-
-static void select_right_row(uint8_t row) {
-    for (uint8_t i = 0; i < 3; i++) {
-        setPinInputHigh(right_row_pins[i]);
-    }
-
-    setPinOutput(right_row_pins[row]);
-    writePinLow(right_row_pins[row]);
-}
-
-static matrix_row_t read_right_cols(void) {
-    matrix_row_t row = 0;
-
-    for (uint8_t col = 0; col < 6; col++) {
-        if (!readPin(right_col_pins[col])) {
-            row |= (1 << col);
-        }
-    }
-
-    return row;
-}
+static const pin_t row_pins[3] = { GP0, GP2, GP4 };
+static const pin_t col_pins[6] = { GP6, GP8, GP10, GP12, GP14, GP26 };
 
 void matrix_init_custom(void) {
-    i2c_init();
-
-    mcp_available = mcp_write(MCP_IODIRA, 0xFF);
-
-    if (mcp_available) {
-        mcp_write(MCP_IODIRB, 0xFF);
-        mcp_write(MCP_GPPUA, 0xFF);
-        mcp_write(MCP_GPPUB, 0xFF);
-        mcp_write(MCP_OLATA, 0xFF);
-        mcp_write(MCP_OLATB, 0xFF);
-    }
-
     for (uint8_t row = 0; row < 3; row++) {
-        setPinInputHigh(right_row_pins[row]);
+        setPinInputHigh(row_pins[row]);
     }
 
     for (uint8_t col = 0; col < 6; col++) {
-        setPinInputHigh(right_col_pins[col]);
+        setPinInputHigh(col_pins[col]);
     }
 }
 
@@ -139,24 +19,25 @@ bool matrix_scan_custom(matrix_row_t current_matrix[]) {
     bool changed = false;
     matrix_row_t next[MATRIX_ROWS] = {0};
 
-    if (mcp_available) {
-        for (uint8_t row = 0; row < 3; row++) {
-            select_left_row(row);
-            wait_us(30);
-            next[row] = read_left_cols();
+    for (uint8_t row = 0; row < 3; row++) {
+        for (uint8_t r = 0; r < 3; r++) {
+            setPinInputHigh(row_pins[r]);
         }
 
-        mcp_all_idle();
-    }
+        setPinOutput(row_pins[row]);
+        writePinLow(row_pins[row]);
 
-    for (uint8_t row = 0; row < 3; row++) {
-        select_right_row(row);
         wait_us(30);
-        next[row + 3] = read_right_cols();
+
+        for (uint8_t col = 0; col < 6; col++) {
+            if (!readPin(col_pins[col])) {
+                next[row + 3] |= (1 << col);
+            }
+        }
     }
 
     for (uint8_t row = 0; row < 3; row++) {
-        setPinInputHigh(right_row_pins[row]);
+        setPinInputHigh(row_pins[row]);
     }
 
     for (uint8_t row = 0; row < MATRIX_ROWS; row++) {
