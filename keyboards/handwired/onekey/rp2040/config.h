1404d854925188f4e7882e5110b0b8ed7262dfe9
Robin Gosse <robingosse2@gmail.com>
2026-05-12 14:44:14 -0230

    Add VIAL configuration defines for keyboard

--- keyboards/handwired/onekey/rp2040/config.h
+++ keyboards/handwired/onekey/rp2040/config.h
@@ -3,6 +3,10 @@
 
 #pragma once
 
+#define VIAL_KEYBOARD_UID {0xD4, 0xC0, 0x00, 0x00, 0x24, 0x05, 0x20, 0x26}
+#define VIAL_UNLOCK_COMBO_ROWS {3, 3}
+#define VIAL_UNLOCK_COMBO_COLS {0, 5}
+
 #define QMK_WAITING_TEST_BUSY_PIN GP8
 #define QMK_WAITING_TEST_YIELD_PIN GP9
 
